9c83e416589d4318d32359af915000f7c4e0d8f2
raditotev <raditotev@gmail.com>
2016-07-18 14:41:22 +0100

    Update breaks controller and tests

--- app/controllers/breaks_controller.rb
+++ app/controllers/breaks_controller.rb
@@ -2,13 +2,11 @@ class BreaksController < AdministrationController
   before_action :set_break, only: [:show, :edit, :update, :destroy]
 
   # GET /breaks
-  # GET /breaks.json
   def index
     @breaks = Break.all
   end
 
   # GET /breaks/1
-  # GET /breaks/1.json
   def show
   end
 
@@ -22,31 +20,25 @@ class BreaksController < AdministrationController
   end
 
   # POST /breaks
-  # POST /breaks.json
   def create
     @break = Break.new(break_params)
 
     respond_to do |format|
       if @break.save
-        format.html { redirect_to @break, notice: 'Break was successfully created.' }
-        format.json { render :show, status: :created, location: @break }
+        format.html { redirect_to @break, flash: {success: 'Break was successfully created.'} }
       else
         format.html { render :new }
-        format.json { render json: @break.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /breaks/1
-  # PATCH/PUT /breaks/1.json
   def update
     respond_to do |format|
       if @break.update(break_params)
-        format.html { redirect_to @break, notice: 'Break was successfully updated.' }
-        format.json { render :show, status: :ok, location: @break }
+        format.html { redirect_to @break, flash: {success: 'Break was successfully updated.'} }
       else
         format.html { render :edit }
-        format.json { render json: @break.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,8 +48,7 @@ class BreaksController < AdministrationController
   def destroy
     @break.destroy
     respond_to do |format|
-      format.html { redirect_to breaks_url, notice: 'Break was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to breaks_url, flash: {success: 'Break was successfully destroyed.'} }
     end
   end
 
