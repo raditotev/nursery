ff1379909cfebb84cce609c53cd71f8f0a4cf8f1
raditotev <raditotev@gmail.com>
2016-07-21 14:48:00 +0100

    Modify assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += [ 'administration.js', 'administration.scss', 'devise.scss' ]
+Rails.application.config.assets.precompile += %w( administration.js administration.scss devise.scss )
+
