5cfef00914106c7306e83d2f8e93f739e1714d62
raditotev <raditotev@gmail.com>
2016-07-25 21:35:07 +0100

    Change routing for all index pages for all resources

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,23 @@
 module AdminsHelper
+  def get_title object
+    title = case
+    when object.class == Faq then object.question
+    when object.class == Break then break_text(object)
+    when object.class == Newsletter then object.name
+    when object.class == Parent then parent_full_name object
+    when object.class == Admin then object.username
+    else
+      object.title
+    end
+  end
+
+  def break_text closure
+    start = closure.start_date
+    finish = closure.end_date
+    "From #{start.strftime("%e %B")} to #{finish.strftime("%e %B %Y")}"
+  end
+
+  def parent_full_name parent
+    "#{parent.title} #{parent.first_name} #{parent.last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-  resources :faqs
-  resources :jobs
-  resources :breaks
-  resources :testemonials
-  resources :awards
-  resources :newsletters
+  resources :faqs, except: :index
+  resources :jobs, except: :index
+  resources :breaks, except: :index
+  resources :testemonials, except: :index
+  resources :awards, except: :index
+  resources :newsletters, except: :index
   devise_for :parents, only: [:sessions, :passwords]
-  resources :parents
+  resources :parents, except: :index
   get 'admin' => 'admin_pages#dashboard'
-  get 'admin/awards' => 'admin_pages#awards'
-  get 'admin/faqs' => 'admin_pages#faqs'
-  get 'admin/breaks' => 'admin_pages#breaks'
-  get 'admin/jobs' => 'admin_pages#jobs'
-  get 'admin/newsletters' => 'admin_pages#newsletters'
-  get 'admin/parents' => 'admin_pages#parents'
-  get 'admin/testemonials' => 'admin_pages#testemonials'
-  get 'admin/admins' => 'admin_pages#admins'
+  get 'admin/awards' => 'awards#index'
+  get 'admin/faqs' => 'faqs#index'
+  get 'admin/breaks' => 'breaks#index'
+  get 'admin/jobs' => 'jobs#index'
+  get 'admin/newsletters' => 'newsletters#index'
+  get 'admin/parents' => 'parents#index'
+  get 'admin/testemonials' => 'testemonials#index'
+  get 'admin/admins' => 'admins#index'
 
   devise_for :admins, only: [:sessions, :passwords]
-  resources :admins
+  resources :admins, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
