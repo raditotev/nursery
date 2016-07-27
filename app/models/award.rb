c59e950dab073926db69fd3d1eda02e9d9b2e865
raditotev <raditotev@gmail.com>
2016-07-27 14:46:48 +0100

    Update award, newsletter models and award form

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,5 +1,5 @@
 class Award < ApplicationRecord
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", dependent: :destroy
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, dependent: :destroy
   validates_attachment_content_type :photo, content_type: ["image/jpeg", "image/gif", "image/png"]
   validates_presence_of :title
 end
--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,6 +1,6 @@
 class Newsletter < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :url
-  has_attached_file :document
+  has_attached_file :document, dependent: :destroy
   validates_attachment :document, content_type: { content_type: "application/pdf" }
 end
