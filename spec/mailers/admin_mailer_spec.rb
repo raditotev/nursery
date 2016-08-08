ca3ed5ed0f3eb668d226e69c1e11be246c372052
raditotev <raditotev@gmail.com>
2016-08-08 13:27:29 +0100

    Update admin mailer test

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -2,38 +2,35 @@ require "rails_helper"
 
 RSpec.describe AdminMailer, type: :mailer do
   describe "send_admin_ password" do
-    before do
-      @admin = create :admin
-    end
-    let(:mail) { AdminMailer.send_admin_password(@admin) }
+
+    let(:admin) { create(:admin) }
+    let(:mail) { AdminMailer.send_admin_password(admin) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Your administrator password")
-      expect(mail.to).to eq([@admin.email])
+      expect(mail.to).to eq([admin.email])
       expect(mail.from).to eq(["admin@oaktreedaynursery.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match @admin.username
-      expect(mail.body.encoded).to match @admin.generated_password
+      expect(mail.body.encoded).to match admin.username
+      expect(mail.body.encoded).to match admin.generated_password
     end
   end
 
   describe "send_parent_ password" do
-    before do
-      @parent = create :parent
-    end
-    let(:mail) { AdminMailer.send_parent_password(@parent) }
+    let(:parent) { create(:parent) }
+    let(:mail) { AdminMailer.send_parent_password(parent) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Your Oaktree Password")
-      expect(mail.to).to eq([@parent.email])
+      expect(mail.to).to eq([parent.email])
       expect(mail.from).to eq(["admin@oaktreedaynursery.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match @parent.email
-      expect(mail.body.encoded).to match @parent.generated_password
+      expect(mail.body.encoded).to match parent.email
+      expect(mail.body.encoded).to match parent.generated_password
     end
   end
 end
