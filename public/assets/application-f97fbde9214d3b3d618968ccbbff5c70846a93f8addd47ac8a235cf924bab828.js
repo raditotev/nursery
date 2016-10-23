d263ca72760da23849b995b85cc47f9165490b42
raditotev <raditotev@gmail.com>
2016-10-23 11:05:16 +0100

    Precompiled assets

--- public/assets/application-f97fbde9214d3b3d618968ccbbff5c70846a93f8addd47ac8a235cf924bab828.js
+++ public/assets/application-f97fbde9214d3b3d618968ccbbff5c70846a93f8addd47ac8a235cf924bab828.js
@@ -31317,22 +31317,6 @@ var o,i,s,a,u;return i=null!=n?n:{},a=i.restorationIdentifier,s=i.restorationDat
   })
 }))
 ;
-(function() {
-
-
-}).call(this);
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 
 
 
@@ -31358,21 +31342,91 @@ $(function() {
   return setTimeout(flashCallback, 3000);
 });
 
-var ready
+
+var ready;
 ready = function(){
-  // Disable send request button if required fields empty
   $(function() {
-    $('#send_request').hover(function() {
-      if($("#request_name").val() != "" &&
-          $("#request_email").val() !="" &&
-          $("#request_date").val() !=""){
-        $('#send_request').prop("disabled", false);
-      } else {
-        $('#send_request').prop("disabled", true);
-      }
+
+    // Underline active link in navbar
+    var url = window.location;
+    $('#navbar-collapse .navbar-nav li a').filter(function() {
+        return this.href == url;
+    }).parent().addClass('active');
+
+    // Show full job description - careers.thml.erb
+    $('.fadeout').click(function(e) {
+      e.preventDefault();
+      var el = $(this)
+      el.parent().css('height', 'auto');
+      el.remove();
     });
+
+    // Email validation
+    var re = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+    var validEmail = false;
+
+    function validateEmail (target){
+      $(target).change(function(){
+        var match = re.test($(this).val());
+        if (!match) {
+          var selector = (target == '#contact_from') ? 'label:first' : 'label:eq(1)'
+          $(selector).append("<span id='invalid-email'> Please enter valid email</span>")
+          $(this).addClass("incorrect");
+          validEmail = false;
+        } else {
+          $('#invalid-email').remove();
+          $(this).removeClass("incorrect");
+          validEmail = true;
+        }
+      });
+    }
+    validateEmail ("#contact_from");
+    validateEmail ("#request_email");
+
+    // Disable send button
+    var disableButton = function(button, field1, field2, field3){
+      $(field1).add(field2).add(field3).on("change keyup", function(){
+        if($(field1).val() != "" && $(field2).val() != "" && $(field3).val() !="" && validEmail){
+          $(button).prop("disabled", false);
+        } else {
+          $(button).prop("disabled", true);
+        }
+      });
+    }
+    // Request Viewing
+    disableButton("#send_request", "#request_name",
+                               "#request_email", "#request_date");
+    // Contact Form
+    disableButton("#send_message", "#contact_from",
+                               "#contact_subject", "#contact_content");
+
     // Date picker for viewing request modal
     $('#request_date').datepicker({dateFormat: 'dd/mm/yy'});
+
+    // Rotate Testimonials
+    function rotateTestimonials(){
+      $('#testimonials li').hide().eq(0).show();
+      function next(){
+        $('#testimonials li:visible').delay(5000).fadeOut('slow',function(){
+          if ($('#testimonials li:visible').length > 0){
+            return
+          }
+          var $next = $(this).next();
+          if ($next.length == 0) { $next = $('#testimonials ul li:first') }
+          $next.fadeIn('slow', next);
+        });
+       }
+      next();
+    }
+    rotateTestimonials();
+
+    // Accordion Footer Links on small screens
+    $('#footer ul li').click(function() {
+      if ($(window).width() < 768){
+        $(this).find(">ul").toggle(1000);
+      }
+    });
+
   });
 }
 
